2136cf473c22de085c704821e5ce14cb1be7111b
ricardo mejias <panswer.rjmm@gmail.com>
2022-07-30 16:35:59 -0400

    update: finished

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const dotenv = require('dotenv');
+
+const envPath = path.resolve(__dirname, './.env');
+if (fs.existsSync(envPath)) {
+    dotenv.config({
+        path: envPath
+    });
+}
+
+const app = require('./server/app');
+
+const staticFiles = path.resolve(__dirname, './www');
+if (!fs.existsSync(staticFiles)) {
+    console.log("The static folder don't found");
+    process.exit(1);
+} else {
+    app.use(express.static(staticFiles));
+}
+
+app.get('/*', (req, res) => {
+    const pathIndex = path.resolve(staticFiles, 'index.html');
+
+    res.sendFile(pathIndex);
+});
+
+const port = process.env.PORT || 3001;
+app.listen(
+    port,
+    err => err ?
+        console.log(err) :
+        console.log(`Server on ${port}`)
+);
